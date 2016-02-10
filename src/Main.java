d687ec426948fe43229ad0f18f18de75766292ba
Akarak <virtuta@yandex.ru>
2016-02-11 00:11:57 +0400

    New changelist

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package lesson01;
+
+/**
+ * User: gkislin
+ * Date: 18.06.2014
+ */
+public class Main {
+    /**
+     * First java program
+     *
+     * @param args : program arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" !gg!   Hello !");
+    }
+}
