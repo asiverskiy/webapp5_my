d585d933a56bb4d63e43ea148f1b4d59e8a04d7e
Akarak <virtuta@yandex.ru>
2016-02-15 20:48:56 +0400

    New changelist

---
+++ src/main/Car.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created by Akarak on 15.02.2016.
+ */
+public class Car {
+    int speed = 100;
+
+    void go() {
+        System.out.println("Go at speed " + speed);
+
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -14,6 +14,8 @@ public class Main {
         // Задать входной параметр args. В меню выбрать Run -> Edit Configurations...-> Program arguments
         // Форматировть код Ctrl + Alt + L
         System.out.println("1:" + "Hello " + args[0] + " !");
-        System.out.format("1:" + "Hello %s ", args[0] + " !");
+        System.out.format("2:" + "Hello %s\n ", args[0] + " !");
+        Car car = new Car();
+        car.go();
     }
 }
