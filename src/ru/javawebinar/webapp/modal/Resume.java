b58b18b5ad31ce9af199866486fddd38666b7534
Akarak <virtuta@yandex.ru>
2016-02-15 20:40:12 +0400

    New changelist

--- src/ru/javawebinar/webapp/modal/Resume.java
+++ src/ru/javawebinar/webapp/modal/Resume.java
@@ -1,9 +1,13 @@
 package ru.javawebinar.webapp.modal;
 
+import java.util.Collection;
+
 /**
  * Created by Akarak on 15.02.2016.
  */
 public class Resume {
     String fullName;
-    String lastName;
+    String [] some;
+    Collection<String> collection;
 }
+
