61213344c8d161fc638104e51b745013e8265ac9
Akarak <virtuta@yandex.ru>
2016-02-21 11:19:38 +0400

    New changelist

---
+++ src/ru/javawebinar/webapp/modal/Link.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.webapp.modal;
+// Настройка Settings -> Edition -> File and Code Templates -> Includes -> File Header
+/**
+ * Akarak
+ * 16.02.2016.
+ */
+public class Link {
+}
